87177fe703dff7c6b552fcae2f64d5444ca2b9bb
JuiusNiedermeier <juius.niedermeier@outlook.de>
2023-05-20 14:33:44 +0200

    Setup Storybook for SolidJS

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "storybook-solidjs-vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../packages/solid/**/*.mdx",
+    "../packages/solid/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "storybook-solidjs-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+
+export default config;
